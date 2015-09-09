ac84ab4c421858deef910d7e1c73d8a77badd477
Simon Walker <s.r.walker101@googlemail.com>
2015-09-09 17:35:24 +0100

    Add common code into ngts_transmission module

--- bin/build_catalogue.py
+++ bin/build_catalogue.py
@@ -5,16 +5,14 @@ from __future__ import division, print_function, absolute_import
 import argparse
 import logging
 from astropy.io import fits
-import pymysql
-from contextlib import contextmanager
 from collections import namedtuple
 import tempfile
 import subprocess as sp
 import numpy as np
 from scipy.spatial import KDTree
 
-logging.basicConfig(level='INFO', format='%(levelname)7s %(message)s')
-logger = logging.getLogger(__name__)
+from ngts_transmission import (logger, connect_to_database,
+                               add_database_arguments)
 
 TransmissionCatalogueEntry = namedtuple('TransmissionCatalogueEntry', [
     'image_id',
@@ -146,26 +144,6 @@ def extract_from_file(fname, region_filename):
             flux_adu=float(row['Aper_flux_3']))
 
 
-@contextmanager
-def connect_to_database(args):
-    if args.db_host is not None:
-        with pymysql.connect(user=args.db_user,
-                            host=args.db_host,
-                            db=args.db_name) as cursor:
-            logger.debug('Connected to database')
-            yield cursor
-            logger.debug('Closing database connection')
-    else:
-        socket = args.db_socket if args.db_socket is not None else '/var/lib/mysql/mysql.sock'
-        with pymysql.connect(user=args.db_user,
-                            unix_socket=socket,
-                            db=args.db_name) as cursor:
-            logger.debug('Connected to database')
-            yield cursor
-            logger.debug('Closing database connection')
-
-
-
 def upload_info(extracted_data, cursor):
 
     query = '''insert into transmission_sources ({fields})
@@ -243,19 +221,6 @@ if __name__ == '__main__':
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('refimage')
-    parser.add_argument('--db-socket',
-                        required=False,
-                        help='Socket to connect to')
-    parser.add_argument('--db-host',
-                        required=False,
-                        help='Host to connect to')
-    parser.add_argument('--db-user',
-                        required=False,
-                        default='ops',
-                        help='Database user')
-    parser.add_argument('--db-name',
-                        required=False,
-                        default='ngts_ops',
-                        help='Database')
+    add_database_arguments(parser)
     parser.add_argument('--fits-out', required=False)
     main(parser.parse_args())
