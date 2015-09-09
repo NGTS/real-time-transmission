74c69c259220a2c1fcbd2fb40e7e990151580e93
Simon Walker <s.r.walker101@googlemail.com>
2015-09-09 11:21:54 +0100

    Add schema initialisation in test script

--- test.sh
+++ test.sh
@@ -3,7 +3,22 @@
 set -eu
 
 clear_database() {
-    mysql -u sw -h ngtsdb ngts_ops -e 'drop table if exists transmission_sources'
+    mysql -u sw -h localhost ngts_ops -e 'drop table if exists transmission_sources'
+}
+
+initialise_schema() {
+    echo Initialising schema
+    mysql -u sw -h localhost ngts_ops -e 'create table transmission_sources (
+    image_id bigint primary key,
+    x_coordinate float not null,
+    y_coordinate float not null,
+    inc_prescan tinyint default 1,
+    flux_adu float not null,
+    foreign key (image_id)
+        references autoguider_refimage(ref_image_id)
+        on delete restrict
+    )
+    '
 }
 
 extract_sources() {
@@ -12,5 +27,8 @@ extract_sources() {
 
 main() {
     clear_database
+    initialise_schema
     extract_sources
 }
+
+main
