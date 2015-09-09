75ef587e48004a246616723974cf9a57ec34dc2e
Simon Walker <s.r.walker101@googlemail.com>
2015-09-09 10:56:07 +0100

    Add initial file to start with

---
+++ bin/build_catalogue.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function, absolute_import
+import argparse
+import logging
+
+logging.basicConfig(level='INFO', format='%(levelname)7s %(message)s')
+logger = logging.getLogger(__name__)
+
+
+def main(args):
+    if args.verbose:
+        logger.setLevel('DEBUG')
+    logger.debug(args)
+
+
+if __name__ == '__main__':
+    description = '''
+    Given an autoguider reference frame, extract a source catalogue, filter the
+    object list and store in a file
+    '''
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('-v', '--verbose', action='store_true')
+    main(parser.parse_args())
