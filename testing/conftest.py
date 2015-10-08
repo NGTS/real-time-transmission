b96fce78445e0465a021a8f76055e0db5821f6fe
Simon Walker <s.r.walker101@googlemail.com>
2015-10-08 17:35:50 +0100

    Add definite valid job into database

--- testing/conftest.py
+++ testing/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 import pymysql
 import os
+import datetime
 
 build_connection = lambda: pymysql.connect(user='ops', db='ngts_ops')
 
@@ -27,5 +28,19 @@ def job_db(job_db_query):
     for line in job_db_query.split(';'):
         if line.strip():
             cursor.execute(line)
+    # Make sure there is one entry always valid
+    past = datetime.datetime.utcnow() - datetime.timedelta(days=1)
+    future = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+    cursor.execute('''
+        insert into job_queue (job_type, submitted, expires)
+        values (%s, %s, %s)
+        ''', ('transmission', past, future))
+    row_id = cursor.lastrowid
+    cursor.execute('''
+        insert into job_args (job_id, arg_key, arg_value)
+        values (%s, %s, %s)
+        ''', (
+        row_id, 'file',
+        '/ngts/das03/action106267_observeField/IMAGE80520150920234004.fits'))
     connection.commit()
     connection.close()
