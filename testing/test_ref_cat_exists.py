b796c52b953b3b244c2ef76dea34159ff859be4f
Simon Walker <s.r.walker101@googlemail.com>
2015-10-09 10:01:36 +0100

    Switch to pytest oriented style for tests

--- testing/test_ref_cat_exists.py
+++ testing/test_ref_cat_exists.py
@@ -1,19 +1,25 @@
 import mock
+import pytest
 from pymysql.cursors import Cursor
 
 from ngts_transmission.watching import ref_catalogue_exists
 
 
-class TestRefCatalogueChecking(object):
+@pytest.fixture
+def cursor():
+    return mock.MagicMock(name='cursor', spec=Cursor)
 
-    def setup(self):
-        self.cursor = mock.MagicMock(name='cursor', spec=Cursor)
-        self.ref_id = 10101
 
-    def test_query_for_reference_ids(self):
-        self.cursor.__iter__.return_value = [(self.ref_id,),]
-        assert ref_catalogue_exists(self.cursor, self.ref_id) == True
+@pytest.fixture
+def ref_id():
+    return 10101
 
-    def test_query_no_ref_catalogue_exists(self):
-        self.cursor.execute.return_value = []
-        assert ref_catalogue_exists(self.cursor, self.ref_id) == False
+
+def test_query_for_reference_ids(cursor, ref_id):
+    cursor.__iter__.return_value = [(ref_id,),]
+    assert ref_catalogue_exists(cursor, ref_id) == True
+
+
+def test_query_no_ref_catalogue_exists(cursor, ref_id):
+    cursor.execute.return_value = []
+    assert ref_catalogue_exists(cursor, ref_id) == False
